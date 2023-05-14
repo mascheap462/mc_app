d45343b2f1039441e2e75742267d04ef1211d5a5
Worldln <anzonx@gmail.com>
2023-05-14 19:14:59 +0200

    Barra de navegación inferior funcional añadida

--- app/src/main/java/com/example/mascheap/MainActivity.java
+++ app/src/main/java/com/example/mascheap/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.mascheap;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -9,13 +12,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.mascheap.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-     Button btn_agregar1;
+
+    ActivityMainBinding binding;
+    Button btn_agregar1;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         //se instancia boton
         btn_agregar1 = findViewById(R.id.btn1);
         //evento boton
@@ -26,7 +35,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
+            switch(item.getItemId()) {
+
+                case R.id.principal:
+                    replaceFragment(new PrincipalFragment());
+                    break;
+                case R.id.buscar:
+                    replaceFragment(new BuscarFragment());
+                    break;
+                case R.id.ofertas:
+                    replaceFragment(new OfertasFragment());
+                    break;
+                case R.id.carritos:
+                    replaceFragment(new CarritosFragment());
+                    break;
+            }
+
+            return true;
+        });
+
+        /* La visibilidad de la barra inferior de navegación dependerá de tener una sesió iniciada
+        * o no. Aunque también puede que para manejarlo lo tengamos en otra Actividad, pero la verdad
+        * es que no tengo ni idea de cómo se hacen aplicaciones Android y estoy un poco harto
+        * así que por ahora está así. */
+        binding.bottomNavigationView.setVisibility(View.VISIBLE);
 
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.linearLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
