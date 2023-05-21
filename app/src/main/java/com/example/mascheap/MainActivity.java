a71cec407d1c844a09876e796e59aee80bf6709f
DanielMayorga <dma0006@alu.medac.es>
2023-05-21 18:01:53 +0200

    agregado icono + splash screen

--- app/src/main/java/com/example/mascheap/MainActivity.java
+++ app/src/main/java/com/example/mascheap/MainActivity.java
@@ -22,21 +22,7 @@ import database.models.callbacks.FirestoreCallbackList;
 import database.models.entities.User;
 
 public class MainActivity extends AppCompatActivity {
-    /*Button btn_agregar1;
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //se instancia boton
-        btn_agregar1 = findViewById(R.id.btn1);
-        //evento boton
-        btn_agregar1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CrearUsuario.class));//para indicar hacia donde queremos ir (Accion de navegar entre ventanas)
-            }
-        });*/
+
     FirebaseAuth auth;
     Button button;
     TextView textView;
