bd79b169871a5cdc95a21a2d96ed71cc2d9efbad
Barbara Sanchez Rueda <simplementebarbara22@gmail.com>
2023-05-28 21:35:01 +0200

    corregido cierre de formulario de oferta vacios

--- app/src/main/java/com/example/mascheap/formularioOfertaFragment.java
+++ app/src/main/java/com/example/mascheap/formularioOfertaFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ public class formularioOfertaFragment extends DialogFragment {
     private EditText producto, precio, supermercado;
 
     public Dialog onCreateDialog(Bundle saveInstanceState){
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
@@ -32,17 +34,35 @@ public class formularioOfertaFragment extends DialogFragment {
                         Toast.makeText(getActivity(), "Accion cancelada", Toast.LENGTH_LONG).show();
                     }
                 })
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getActivity(), "¡Gracias por su aportacion!", Toast.LENGTH_LONG).show();
-                    }
-                });
+                .setPositiveButton("Ok", null);  // Set null para evitar el cierre automático
+
         producto = view.findViewById(R.id.nombreOferta);
         precio = view.findViewById(R.id.precioOferta);
         supermercado = view.findViewById(R.id.supermercadoOferta);
 
-        return builder.create();
-    }
+        final AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button okButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String nombreProducto = producto.getText().toString();
+                        String precioOferta = precio.getText().toString();
+                        String nombreSupermercado = supermercado.getText().toString();
+                        if (nombreProducto.isEmpty() || precioOferta.isEmpty() || nombreSupermercado.isEmpty()) {
+                            Toast.makeText(getActivity(), "Por favor, complete todos los campos", Toast.LENGTH_LONG).show();
+                        } else {
+                            // Realizar la acción deseada, ya que los campos no están vacíos
+                            Toast.makeText(getActivity(), "¡Gracias por su aportación!", Toast.LENGTH_LONG).show();
+                            dialog.dismiss();  // Cerrar el diálogo manualmente después de la acción deseada
+                        }
+                    }
+                });
+            }
+        });
 
+        return dialog;
+    }
 }
