b3fedf8add6b6bd84d08e51538bd6bd903754e32
DanielMayorga <dma0006@alu.medac.es>
2023-05-28 20:19:17 +0200

    capa de pintura + foto usuario

--- app/src/main/java/com/example/mascheap/MainActivity.java
+++ app/src/main/java/com/example/mascheap/MainActivity.java
@@ -123,7 +123,8 @@ public class MainActivity extends AppCompatActivity {
                     replaceFragment(new CarritosFragment());
                     break;
             }
-
+            desactivarBoton(btnRegreso);
+            activarBoton(btnAjustes);
             return true;
         });
 
